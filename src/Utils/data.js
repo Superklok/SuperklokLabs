38f9a1b3dd22d0497ae49905d332da7f0baff883
Trev Morin <trev@superklok.com>
2023-10-16 23:02:27 -0500

    Add Next App

---
+++ src/Utils/data.js
@@ -0,0 +1,222 @@
+export const HomeTracks = [
+  {
+    src: "Home/Track1.jpg",
+    bg: "#00003D",
+    delay: 0.1,
+  },
+  {
+    src: "Home/Track2.jpg",
+    bg: "#00003D",
+    delay: 0.3,
+  },
+  {
+    src: "Home/Track3.jpg",
+    bg: "#00003D",
+    delay: 0.2,
+  },
+  {
+    src: "Home/Track4.jpg",
+    bg: "#00003D",
+    delay: 0.2,
+  },
+  {
+    src: "Home/Track5.jpg",
+    bg: "#00003D",
+    delay: 0.3,
+  },
+  {
+    src: "Home/Track6.jpg",
+    bg: "#00003D",
+    delay: 0.25,
+  },
+];
+
+export const Services = [
+  {
+    icon: "/Services/Frame-0.png",
+    title: "React.js",
+  },
+  {
+    icon: "/Services/Frame-1.png",
+    title: "Next.js",
+  },
+  {
+    icon: "/Services/Frame-2.png",
+    title: "JavaScript ES6",
+  },
+  {
+    icon: "/Services/Frame-3.png",
+    title: "FL Studio",
+  },
+  {
+    icon: "/Services/Frame-4.png",
+    title: "Adobe After Effects",
+  },
+  {
+    icon: "/Services/Frame-5.png",
+    title: "DxO PhotoLab",
+  },
+];
+
+export const Connect = [
+  {
+    icon: "/Connect/Frame-0.png",
+    title: "GitHub",
+    desc: "All Superklok Labs repositories are publicly available on our GitHub!",
+  },
+  {
+    icon: "/Connect/Frame-1.png",
+    title: "YouTube",
+    desc: "Check out the 4K music and gaming content on our YouTube channel!",
+  },
+  {
+    icon: "/Connect/Frame-2.png",
+    title: "TikTok",
+    desc: "Follow us on TikTok to see our LIVE streams and exclusive content!",
+  },
+];
+
+export const DevOps = [
+  {
+    icon: "/DevOps/Frame-0.png",
+    title: "Docker",
+    desc: "Enhancing legacy apps by turning them into images then capturing running instances of them called containers using Docker. This increases scalability and facilitates cross-platform compatibility.",
+  },
+  {
+    icon: "/DevOps/Frame-1.png",
+    title: "Kubernetes",
+    desc: "A great way to manage your containers is by orchestrating them in pods using Kubernetes. This allows you to store secrets, obtain SSL certificates, and quickly scale up an app across many nodes.",
+  },
+  {
+    icon: "/DevOps/Frame-2.png",
+    title: "Amazon Web Services",
+    desc: "Deploying the full AWS DevOps arsenal of tools to tackle all your DevOps Engineering needs.",
+  },
+];
+
+export const About = [
+  {
+    title: "The Company",
+    desc: "Superklok Labs is an IT consulting firm that specializes in JavaScript, React.js, Next.js, AWS DevOps Engineering, PC building, French/English translation, music production, and graphic design.",
+  },
+  {
+    title: "Our Mission",
+    desc: "Conceptualizing then creating JavaScript applications, custom PCs, music, graphics, and videos that stand out thanks to unique design choices.",
+  },
+  {
+    title: "Fully Bilingual",
+    desc: "We're a Canadian business so all our consultations are offered in both English and French.",
+  },
+  {
+    title: "Questions?",
+    desc: "Please reach out to info@superklok.com with any general enquiries and contact sales@superklok.com to book a consultation.",
+  },
+];
+
+export const Containers = [
+  {
+    icon: "/Containers/Frame-0.jpg",
+    title: "Swingcars (English)",
+    desc: "Swingcars is a social media app intended to facilitate vehicle swapping between drivers.",
+    docker: "https://hub.docker.com/r/superklok/swingcarsdotnet",
+    github: "https://github.com/Superklok/SwingcarsDotNet",
+  },
+  {
+    icon: "/Containers/Frame-1.jpg",
+    title: "The Current Courant (English)",
+    desc: "The Current Courant is newspaper app that allows users to publish their own articles.",
+    docker: "https://hub.docker.com/r/superklok/currentcourantdotcom",
+    github: "https://github.com/Superklok/CurrentCourantDotCom",
+  },
+  {
+    icon: "/Containers/Frame-2.jpg",
+    title: "Jet Wheel Reel (English)",
+    desc: "Jet Wheel Reel is an educational site that demonstrates the correct way to carry out a wheel change procedure on a Beechcraft 1900D.",
+    docker: "https://hub.docker.com/r/superklok/jetwheelreeldotcom",
+    github: "https://github.com/Superklok/JetWheelReelDotCom",
+  },
+  {
+    icon: "/Containers/Frame-3.jpg",
+    title: "Slider Piler (English)",
+    desc: "Slider Piler is an online ordering app which currently consists of a create-your-own sandwich builder.",
+    docker: "https://hub.docker.com/r/superklok/sliderpilerdotcom",
+    github: "https://github.com/Superklok/SliderPilerDotCom",
+  },
+  {
+    icon: "/Containers/Frame-4.jpg",
+    title: "TrevMorin.com (English)",
+    desc: "TrevMorin.com is a portfolio website that showcases each web design concept created by Superklok Labs.",
+    docker: "https://hub.docker.com/r/superklok/trevmorindotcom",
+    github: "https://github.com/Superklok/TrevMorinDotCom",
+  },
+  {
+    icon: "/Containers/Frame-5.jpg",
+    title: "Superklok Labs (English)",
+    desc: "The Superklok Labs company website provides a technical and detailed description of each available service.",
+    docker: "https://hub.docker.com/r/superklok/superklokdotcom",
+    github: "https://github.com/Superklok/SuperklokDotCom",
+  },
+  {
+    icon: "/Containers/Frame-6.jpg",
+    title: "React Crypto Stats (English)",
+    desc: "Crypto Stats is a cryptocurrency statistical analysis app that uses Chart.js to illustrate market performance.",
+    docker: "https://hub.docker.com/r/superklok/reactcryptostats",
+    github: "https://github.com/Superklok/ReactCryptoStats",
+  },
+  {
+    icon: "/Containers/Frame-7.jpg",
+    title: "React Candy Crush (English)",
+    desc: "This is a React replica of Candy Crush for Desktop/Laptop that was programmed with the useState and useEffect React Hooks.",
+    docker: "https://hub.docker.com/r/superklok/reactcandycrush",
+    github: "https://github.com/Superklok/ReactCandyCrush",
+  },
+  {
+    icon: "/Containers/Frame-8.jpg",
+    title: "React Task Tracker (English)",
+    desc: "A to-do list that demonstrates CRUD functionality. V1.19.1 uses a database and the demo, v2.2.1, uses the DOM.",
+    docker: "https://hub.docker.com/r/superklok/reacttasktracker",
+    github: "https://github.com/Superklok/ReactTaskTracker",
+  },
+  {
+    icon: "/Containers/Frame-9.jpg",
+    title: "Swingcars (Version française)",
+    desc: "Swingcars est une appli de médias sociaux destinée à faciliter l'échange de véhicules entre conducteurs.",
+    docker: "https://hub.docker.com/r/superklok/swingcarspointca",
+    github: "https://github.com/Superklok/SwingcarsPointCa",
+  },
+  {
+    icon: "/Containers/Frame-10.jpg",
+    title: "Le Current Courant (Version française)",
+    desc: "Le Current Courant est une appli journalistique qui permet aux utilisateurs de publier leurs propres articles.",
+    docker: "https://hub.docker.com/r/superklok/currentcourantpointca",
+    github: "https://github.com/Superklok/CurrentCourantPointCa",
+  },
+  {
+    icon: "/Containers/Frame-11.jpg",
+    title: "Jet Wheel Reel (Version française)",
+    desc: "Jet Wheel Reel est un site éducatif qui montre la manière correcte d'effectuer une procédure de changement de roue sur un Beechcraft 1900D.",
+    docker: "https://hub.docker.com/r/superklok/jetwheelreelpointca",
+    github: "https://github.com/Superklok/JetWheelReelPointCa",
+  },
+  {
+    icon: "/Containers/Frame-12.jpg",
+    title: "Slider Piler (Version française)",
+    desc: "Slider Piler est une application de resto qui consiste actuellement d'un outil de création de sandwichs à la vôtre.",
+    docker: "https://hub.docker.com/r/superklok/sliderpilerpointca",
+    github: "https://github.com/Superklok/SliderPilerPointCa",
+  },
+  {
+    icon: "/Containers/Frame-13.jpg",
+    title: "TrevMorin.ca (Version française)",
+    desc: "TrevMorin.ca est un site web de portfolio qui présente chaque concept d'appli créé par Superklok Labs.",
+    docker: "https://hub.docker.com/r/superklok/trevmorinpointca",
+    github: "https://github.com/Superklok/TrevMorinPointCa",
+  },
+  {
+    icon: "/Containers/Frame-14.jpg",
+    title: "Superklok Labs (Version française)",
+    desc: "Le site web de l'entreprise Superklok Labs fournit une description technique et détaillée de chaque service disponible.",
+    docker: "https://hub.docker.com/r/superklok/superklokpointca",
+    github: "https://github.com/Superklok/SuperklokPointCa",
+  },
+];
