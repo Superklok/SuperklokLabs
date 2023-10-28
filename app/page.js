25c9b26e1f4ae8d9d72574330e3de0c992e6f977
Trev Morin <trev@superklok.com>
2023-10-27 20:57:06 -0500

    Add Connect

--- app/page.js
+++ app/page.js
@@ -1,16 +1,33 @@
-import { Navbar } from '@/src/components/Navbar/Navbar';
+'use client'
+
+import {Navbar} from '@/src/components/Navbar/Navbar';
 import Landing from '@/src/components/Landing/Landing';
 import PromoVid from '@/src/components/PromoVid/PromoVid';
 import OurServices from '@/src/components/OurServices/OurServices';
+import Connect from '@/src/components/Connect/Connect';
+import {motion, useAnimation} from 'framer-motion';
 import './page.css';
 
 export default function Home() {
+	const controls = useAnimation();
+
 	return (
-		<div className='app'>
+		<motion.div className='app' animate={controls}>
 			<Navbar/>
 			<Landing/>
 			<PromoVid/>
 			<OurServices/>
-		</div>
+			<motion.div
+			onViewportEnter={()=> controls.start({
+				backgroundColor: 'var(--secondary-color)'
+			})}
+			onViewportLeave={()=> controls.start({
+				backgroundColor: 'var(--background)'
+			})}
+			viewport={{amount: 0.4}}
+			>
+				<Connect/>
+			</motion.div>
+		</motion.div>
 	);
 }
---
+++ src/components/Connect/Connect.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Image from 'next/image';
+import {ConnectFeatures} from '@/src/Utils/data';
+import {motion} from 'framer-motion';
+import {containerVariants, descVariants, tagVariants, titleVariants} from '@/src/Utils/animation';
+import './Connect.css';
+
+const Connect = () => {
+	return (
+		<div className='c-wrapper'>
+			<div className='container'>
+				<div className='c-container'>
+
+					{/* Head */}
+					<div className='c-head'>
+						<motion.span 
+						variants={tagVariants}
+						initial='offscreen'
+						whileInView={'onscreen'}
+						className='tag'>Connect With Us</motion.span>
+						<motion.span 
+						initial='offscreen'
+						whileInView={'onscreen'}
+						variants={titleVariants}
+						className='title'>For The Latest Superklok Labs Releases</motion.span>
+						<motion.span 
+						initial='offscreen'
+						whileInView={'onscreen'}
+						variants={descVariants}
+						className='text'>Our mission is to level the playing field for aspiring coders and musicians. We make sure talented individuals have all the premium self-serve resources they need to make their own music or code an app. All repositories, containers, and Superklok music tracks are available to everyone free of charge.</motion.span>
+					</div>
+
+					{/* Features */}
+					<div className='c-features'>
+						{
+							ConnectFeatures.map((feature, i)=> (
+								<motion.div 
+								initial='offscreen'
+								whileInView='onscreen'
+								variants={containerVariants((i+1)*0.1)}
+								key={i} 
+								className='c-feature'>
+									<a href={feature.link}>
+										<Image 
+										className='c-icon'
+										src={feature.icon} 
+										alt='feature' 
+										width={128} 
+										height={128}/>
+									</a>
+									<span className='c-title'>{feature.title}</span>
+									<span className='text'>{feature.desc}</span>
+								</motion.div>
+							))
+						}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Connect;
